eb02ad14f01e38617faf076e2030cefe3b59be42
Renatocirilo <renato_cirilo@hotmail.com>
2019-10-22 22:36:02 -0300

    form email

--- contactform/contactform.js
+++
@@ -1,8 +0,0 @@
-$("#my-form").submit(function(e) {
-  e.preventDefault();
-
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Obrigado pela mensagem, em breve estarei respondendo");
-  });
-});
