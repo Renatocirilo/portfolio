2fa0b74a920c13748f99b4772c06cc9c1ec1c67c
Renatocirilo <renato_cirilo@hotmail.com>
2019-10-22 22:20:59 -0300

    form email

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,25 +1,8 @@
-$(function($) {
-  $("form").submit(function(event) {
-    event.preventDefault();
+$("form").submit(function(e) {
+  e.preventDefault();
 
-    $.ajax({
-      url: "https://getform.io/f/911f664a-c94a-441a-8f35-80759d57fb97",
-      method: "post",
-      data: {
-        name: $("name").val(),
-        email: $("email").val(),
-        message: $("message").val()
-      },
-      dataType: "json"
-    })
-      .done(function() {
-        $("name").val("");
-        $("email").val("");
-        $("message").val("");
-        alert("E-mail enviado com sucesso!");
-      })
-      .fail(function() {
-        alert("Erro ao enviar e-mail!");
-      });
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    alert("Thank you!");
   });
 });
